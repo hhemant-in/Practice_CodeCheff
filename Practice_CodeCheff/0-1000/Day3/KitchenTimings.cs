0d66d046358c69366e08e67f78840bc9a1a07ab1
Hemant <info@hhemant.in>
2023-08-09 13:01:17 +0530

    Task - 16 KitchenTimings
    Day 3

---
+++ Practice_CodeCheff/0-1000/Day3/KitchenTimings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_CodeCheff._0_1000.Day3
+{
+    internal class KitchenTimings
+    {
+        internal void Start()
+        {
+            int NumberOfTestCases = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < NumberOfTestCases; i++)
+            {
+                string[] InputCase = Console.ReadLine().Split();
+                int requiredDonations = int.Parse(InputCase[0]);
+                int collectedDonations = int.Parse(InputCase[1]);
+
+                Console.WriteLine(requiredDonations - collectedDonations);
+            }
+        }
+    }
+}
--- Practice_CodeCheff/Program.cs
+++ Practice_CodeCheff/Program.cs
@@ -13,9 +13,13 @@ namespace Practice_CodeCheff
     {
         static void Main(string[] args)
         {
+            //P16 DL - 273
+            KitchenTimings kitchenTimings = new KitchenTimings();
+            kitchenTimings.Start();
+
             //P15 DL - 272
-            DonationDrive donationDrive = new DonationDrive();
-            donationDrive.Start();
+            //DonationDrive donationDrive = new DonationDrive();
+            //donationDrive.Start();
 
             //P14 DL - 267
             //DetermineTheScore determineTheScore = new DetermineTheScore();
